cc741d5850063d8f95b9c4a7b9fcafb3ff167e9d
Isaac-UTRGV <isaac.castillo08@utrgv.edu>
2026-04-10 06:18:18 +0000

    Added Registration (work in progress)

--- lawnApp/urls.py
+++ lawnApp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', auth_views.LoginView.as_view(template_name='lawnApp/login.html'), name='login'),
     path('login/', auth_views.LoginView.as_view(template_name='lawnApp/login.html'), name='login'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
 ]
